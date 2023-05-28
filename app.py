66c7c6b96b05fc53cabdcf5fd613e36be5f06739
Luis Martinez <luis.martinez33@protonmail.com>
2023-05-28 12:01:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 import datetime
 import json
+from translate import Translator
+from unicodedata import normalize
+import re
+
 app = Flask(__name__)
 
 
@@ -19,6 +23,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET",'POST'])
 def index():
+
+    translator= Translator(to_lang="Spanish")
+
     classifier = joblib.load('./model.pkl')
     predicciones2 = classifier.predict(steps=57)
 
@@ -26,7 +33,12 @@ def index():
     dateDict  ={}
     for x in range(0, 5):
         day = base - datetime.timedelta(days=x)
-        dateDict[day.strftime('%A')] =predicciones2[str(day)]
+        translation = translator.translate(day.strftime('%A'))
+        s = re.sub(
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1",
+        normalize( "NFD", translation), 0, re.I
+    )
+        dateDict[s] =predicciones2[str(day)]
     return json.dumps(dateDict)
 
 
